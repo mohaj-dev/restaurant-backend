ec3d427a782f4d4abd169400db06ce748b78aa6e
mohaj-dev <mohajali91@gmail.com>
2026-04-16 03:52:44 +0200

    import-data1

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
-import { exec } from "child_process";
-
 export default {
-  async bootstrap() {
-    console.log("🔥 Importing data...");
+  register() {},
 
-    exec("npx strapi import -f export_20260416033338.tar --force", (err, stdout, stderr) => {
-      if (err) {
-        console.error("Import error:", err);
-        return;
-      }
-      console.log(stdout);
-      console.error(stderr);
-    });
-  },
+  bootstrap() {},
 };
